8cdd5337bb7b96f63d345aa49c1ba4fe19a1e43e
sogatanco <adeliapucha46@gmail.com>
2023-01-07 14:03:53 +0700

    create home component

--- resources/frontend/angular/src/app/app-routing.module.ts
+++ resources/frontend/angular/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CHomeComponent } from './c-home/c-home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:CHomeComponent},
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- resources/frontend/angular/src/app/app.module.ts
+++ resources/frontend/angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CHomeComponent } from './c-home/c-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CHomeComponent
   ],
   imports: [
     BrowserModule,
